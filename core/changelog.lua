d927eea36b7d8f92d8ae8523b11fa144faf607b2
James Dunlop <kryptick@iinet.net.au>
2023-06-08 00:06:56 +1000

    line breaks at start

--- core/changelog.lua
+++ core/changelog.lua
@@ -1,18 +1,17 @@
-MRADIAL_ABOUT  = "Welcome to mRadial a little UI for tracking spell timers etc. \n \
-You have a Primary and a Secondary radial frame to assign spells to watch to. These can be set to be clickable buttons as well if so desired.\n \
-Why did I write this? Well I like to keep my focus on my character as much as possible and this addon helps keep the most important spells I want to track in \n \
-a nice tight circle around the charcter (so I don't stand in fire mkay!) \n \
-For more info please visit the wiki from the curseForge addon here: \n \
-https://github.com/jamesbdunlop/mRadial/wiki \n"
+MRADIAL_ABOUT  = "Welcome to mRadial a little UI for tracking spell timers etc.  \
+\n You have a Primary and a Secondary radial frame to assign spells to watch to. These can be set to be clickable buttons as well if so desired. \
+\n Why did I write this? Well I like to keep my focus on my character as much as possible and this addon helps keep the most important spells I want to track in  \
+a nice tight circle around the charcter (so I don't stand in fire mkay!)  \
+\n For more info please visit the wiki from the curseForge addon here:  \
+\n https://github.com/jamesbdunlop/mRadial/wiki \n"
 
 MRADIAL_UPDATENOTES = " \
 -- v0.4.01 --\n \
-This overhauls the options pane to use the Ace3ConfigTables. \n \
-A few things of note here: \n \
-If you find the spell icons have gone a bit crazy, this is most likely due to the autoSpread \
-being turned ON by default now. The autospread option tries to make adding/removing spell watchers \
-less problematic, so you don't have to always adjust the spread when adding a new spell. So set any dimensions back to their defaults, and work from there. \n \
-- Primary / Secondary spell orders are now in their own frame (to avoid issues with the ActionSlot frame type) that can be opened using the buttons in the spell selection tab. \
+This overhauls the options pane to use the Ace3ConfigTables. Wiki not yet updated to reflect these changes! \
+\nA few things of note here: \
+\nIf you find the spell icons have gone a bit crazy, this is most likely due to the autoSpread being turned ON by default now. The autospread option tries to make adding/removing spell watchers \
+less problematic, so you don't have to always adjust the spread when adding a new spell. So set any dimensions back to their defaults, and work from there.\
+\n- Primary / Secondary spell orders are now in their own frame (to avoid issues with the ActionSlot frame type) that can be opened using the buttons in the spell selection tab. \
 \n -- IMPROVEMENTS \
 \n - Well the entire options pane for starters. \
 \n - Warlock specific options are now broken out to their own tab. \
