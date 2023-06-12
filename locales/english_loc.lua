ac75fc475730f0822ad03d332804d01a20cdc5f6
James Dunlop <kryptick@iinet.net.au>
2023-06-12 14:23:59 +1000

    added files

---
+++ locales/english_loc.lua
@@ -0,0 +1,124 @@
+-- English localization file for enUS and enGB.
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0");
+local L = AceLocale:NewLocale("mRadial", "enUS", true, true);
+if not L then return; end
+
+L["Opt_GenericOptions_name"] = "Generic Options"
+L["Opt_GenericOptions_desc"] = "These apply to the entire UI"
+
+L["Opt_ConfigMode_name"] = "Config Mode"
+L["Opt_ConfigMode_desc"] = "Enables / disables config mode."
+
+L["Opt_HideOutOfCombat_name"] = "Hide Out Of Combat"
+L["Opt_HideOutOfCombat_desc"] = "Enables / disables hiding the UI while out of combat."
+
+L["Opt_HideMiniMapIcon_name"] = "Hide MiniMap Icon"
+L["Opt_HideMiniMapIcon_desc"] = "Enables / disables miniMapIcon in fav of the new addon container. This change will take effect on next reload/login."
+
+L["Opt_ABOUT_name"] = "ABOUT"
+L["Opt_ABOUT_desc"] =  "Welcome to mRadial a little UI for tracking spell timers etc.  \
+\nYou have a Primary and a Secondary radial frame to assign spells to watch to. These can be set to be clickable buttons as well if so desired. \
+\nWhy did I write this? Well I like to keep my focus on my character as much as possible and this addon helps keep the most important spells I want to track in  \
+a nice tight circle around the charcter (so I don't stand in fire mkay!)  \
+\nFor more info please visit the wiki via curseForge.  \
+\n \n"
+
+L["Opt_CHANGELOG_name"] = "CHANGE LOG"
+
+L["Opt_RadialIcons_name"] = "Radial Icons"
+L["Opt_RadialIcons_desc"] = "Use this to change the spell icons and various text timers."
+
+L["Opt_BaseIconOptions_name"] = "Base Icon Options"
+L["Opt_BaseIconOptions_desc"] = "Use these options to affect the display of the 'watcher' icons."
+
+L["Opt_AutoSpread_name"] = "Auto Spread"
+L["Opt_AutoSpread_desc"] = "Enables / disables an auto spread around the radial frame."
+
+L["Opt_AsButtons_name"] = "As Buttons"
+L["Opt_AsButtons_desc"] =  "Enables / disables the icons as buttons. DEFAULT: False"
+
+L["Opt_Dimensions_name"] = "Dimensions"
+
+L["Opt_RadiusMultiplyer_name"] = "Radius Multiplyer"
+L["Opt_RadiusMultiplyer_desc"] = "Use this to apply a global change to the radius size. DEFAULT: %100."
+
+L["Opt_SpellIconSize_name"] = "Spell Icon Size"
+L["Opt_SpellIconSize_desc"] = "Use this change the spell icon sizes.  DEFAULT: 40"
+
+L["Opt_Primary_name"] = "Primary"
+
+L["Opt_Radius_name"] = "Radius"
+L["Opt_Radius_desc"] = "Use this change the size(radius) of the circle.  DEFAULT: 50"
+
+L["Opt_Spread_name"] = "Spread"
+L["Opt_Spread_desc"] = "Use this change the spell icon spread around the circle."
+
+L["Opt_Offset_name"] = "Offset"
+L["Opt_Offset_desc"] = "Use this offset the start location of the spell icons around the circle. DEFAULT: .7"
+
+L["Opt_WidthAdjust_name"] = "Width Adjust"
+L["Opt_WidthAdjust_desc"] = "Use this offset width of the circle. This will create an oval shape horizontally. DEFAULT: 1"
+
+L["Opt_HeightAdjust_name"] = "Height Adjust"
+L["Opt_HeightAdjust_desc"] = "Use this offset height of the circle. This will create an oval shape vertically. DEFAULT: 1"
+
+L["Opt_Text_name"] = "Text/Fonts"
+
+L["Opt_Font_name"] = "Font"
+
+L["Opt_HeightAdjust_name"] = "Height Adjust"
+L["Opt_HeightAdjust_desc"] = "Use this offset height of the circle. This will create an oval shape vertically. DEFAULT: 1"
+
+L["Opt_GlobalFont%_name"] = "Global Font %"
+L["Opt_GlobalFont%_desc"] = "This controls the overall font size for the UI. A setting of 50% will be 50% of the current icon size.  DEFAULT: %50"
+
+L["Opt_FontSizeOverrides_name"] = "Font Size Overrides"
+
+L["Opt_count_name"] = "Count"
+L["Opt_ready_name"] = "Ready"
+L["Opt_CoolDown_name"] = "Cool Down"
+L["Opt_timer_name"] = "Timer"
+L["Opt_linked_name"] = "Linked"
+L["Opt_fontAdjustWarning_name"] = "--- Turn on Config Mode While Adjusting The Below Params ---"
+
+L["Opt_DEFAULT2_desc"] = "DEFAULT: 2"
+
+L["Opt_radialSpells_desc"] = "Radial Spells"
+
+L["Opt_fagDesc_desc"] = "--Use this to select the spells to watch in the primary and secondary radial frames.--"
+
+L["Opt_hidePassiveSpells_name"] = "Hide Passive Spells"
+
+L["Opt_primarySpellOrder_name"] = "Primary Spell Order"
+L["Opt_primarySpells_name"] = "Primary Spells"
+L["Opt_secondarySpellOrder_name"] = "Secondary Spell Order"
+L["Opt_secondarySpells_name"] = "Secondary Spells"
+
+L["Opt_linkedSpells_name"] = "Linked Spells"
+
+L["Opt_warlock_name"] = "Warlock"
+
+L["Opt_PetFrames_name"] = "Pet Frames"
+L["Opt_HidePetFrames_name"] = "Hide Pet Frames"
+L["Opt_HidePetFrames_desc"] = "Show/Hide the warlock pet frames."
+
+L["Opt_IconSize_name"] = "Icon Size"
+L["Opt_IconSize_desc"] = "Use this change the spell icon sizes."
+
+L["Opt_ShardFrames_name"] = "Shard Frames"
+
+L["Opt_OutOfShardsFrameSize_name"] = "Out Of Shards Frame Size"
+L["Opt_OutOfShardsFrameSize_desc"] = "Use this change the frame that shows the out of shards circle. Turn on Config mode for this!"
+
+L["Opt_ShardTrackerFrameSize_name"] = "Shards Frame Size"
+L["Opt_ShardTrackerFrameSize_desc"] = "Use this change the frame that shows the custom shard tracker frame."
+
+L["Opt_ShardTrackerTrans_name"] = "Shards Frame Transparency"
+L["Opt_ShardTrackerTrans_desc"] = "Use this change alpha of the shard frame. Turn on Config mode for this."
+
+L["Opt_HideShardTracker_name"] = "Hide Shard Frame"
+L["Opt_HideShardTracker_desc"] = "Show/Hide the warlock shard tracker frame."
+
+L["Opt_RDY_name"] = "RDY"
+L["Opt_NOSSSTR_name"] = "N/A"
+L["Opt_OOR_name"] = "OOR"
---
+++ locales/german_loc.lua
@@ -0,0 +1,123 @@
+-- English localization file for enUS and enGB.
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0");
+local L = AceLocale:NewLocale("mRadial", "deDE", true, true);
+if not L then return; end
+
+L["Opt_GenericOptions_name"] = "Allgemeine Optionen"
+L["Opt_GenericOptions_desc"] = "Diese gelten für die gesamte Benutzeroberfläche"
+
+L["Opt_ConfigMode_name"] = "Konfigurationsmodus"
+L["Opt_ConfigMode_desc"] = "Aktiviert/Deaktiviert den Konfigurationsmodus."
+
+L["Opt_HideOutOfCombat_name"] = "Außerhalb des Kampfes ausblenden"
+L["Opt_HideOutOfCombat_desc"] = "Aktiviert/Deaktiviert das Ausblenden der Benutzeroberfläche außerhalb des Kampfes."
+
+L["Opt_HideMiniMapIcon_name"] = "Minikarten-Symbol ausblenden"
+L["Opt_HideMiniMapIcon_desc"] = "Aktiviert/Deaktiviert das Minikarten-Symbol zugunsten des neuen Addon-Behälters. Diese Änderung tritt nach dem nächsten Neuladen/Anmelden in Kraft."
+
+L["Opt_ABOUT_name"] = "ÜBER"
+L["Opt_ABOUT_desc"] = "Willkommen bei mRadial, einer kleinen Benutzeroberfläche zum Verfolgen von Zauber-Timern usw. \
+\nSie haben einen primären und einen sekundären radialen Rahmen, um Zauber zur Beobachtung zuzuweisen. Diese können bei Bedarf auch als anklickbare Schaltflächen festgelegt werden. \
+\nWarum habe ich das geschrieben? Nun, ich möchte meinen Fokus so weit wie möglich auf meinen Charakter halten, und dieses Addon hilft dabei, die wichtigsten Zauber, die ich verfolgen möchte, in einem schönen, engen Kreis um den Charakter herum zu behalten (damit ich nicht im Feuer stehe, okay!) \
+\nFür weitere Informationen besuchen Sie bitte das Wiki über CurseForge. \
+\n \n"
+
+L["Opt_CHANGELOG_name"] = "ÄNDERUNGSPROTOKOLL"
+
+L["Opt_RadialIcons_name"] = "Radiale Symbole"
+L["Opt_RadialIcons_desc"] = "Verwenden Sie dies, um die Zauber-Symbole und verschiedene Text-Timer zu ändern."
+
+L["Opt_BaseIconOptions_name"] = "Basis-Symboloptionen"
+L["Opt_BaseIconOptions_desc"] = "Verwenden Sie diese Optionen, um die Anzeige der 'Watcher'-Symbole zu beeinflussen."
+
+L["Opt_AutoSpread_name"] = "Automatische Verteilung"
+L["Opt_AutoSpread_desc"] = "Aktiviert/Deaktiviert eine automatische Verteilung um den radialen Rahmen."
+
+L["Opt_AsButtons_name"] = "Als Schaltflächen"
+L["Opt_AsButtons_desc"] = "Aktiviert/Deaktiviert die Symbole als Schaltflächen. STANDARD: Falsch"
+
+L["Opt_Dimensions_name"] = "Abmessungen"
+
+L["Opt_RadiusMultiplyer_name"] = "Radius-Multiplikator"
+L["Opt_RadiusMultiplyer_desc"] = "Verwenden Sie dies, um eine globale Änderung der Radiusgröße vorzunehmen. STANDARD: %100."
+
+L["Opt_SpellIconSize_name"] = "Zauber-Symbolgröße"
+L["Opt_SpellIconSize_desc"] = "Verwenden Sie dies, um die Größe der Zauber-Symbole zu ändern. STANDARD: 40"
+
+L["Opt_Primary_name"] = "Primär"
+
+L["Opt_Radius_name"] = "Radius"
+L["Opt_Radius_desc"] = "Verwenden Sie dies, um die Größe (Radius) des Kreises zu ändern. STANDARD: 50"
+
+L["Opt_Spread_name"] = "Verteilung"
+L["Opt_Spread_desc"] = "Verwenden Sie dies, um die Verteilung der Zauber-Symbole um den Kreis zu ändern."
+
+L["Opt_Offset_name"] = "Versatz"
+L["Opt_Offset_desc"] = "Verwenden Sie dies, um den Startort der Zauber-Symbole um den Kreis herum zu versetzen. STANDARD: .7"
+
+L["Opt_WidthAdjust_name"] = "Breitenanpassung"
+L["Opt_WidthAdjust_desc"] = "Verwenden Sie dies, um die Breite des Kreises zu ändern. Dadurch entsteht horizontal eine ovale Form. STANDARD: 1"
+
+L["Opt_HeightAdjust_name"] = "Höhenanpassung"
+L["Opt_HeightAdjust_desc"] = "Verwenden Sie dies, um die Höhe des Kreises zu ändern. Dadurch entsteht vertikal eine ovale Form. STANDARD: 1"
+
+L["Opt_Text_name"] = "Text/Schriften"
+
+L["Opt_Font_name"] = "Schriftart"
+
+L["Opt_HeightAdjust_name"] = "Höhenanpassung"
+L["Opt_HeightAdjust_desc"] = "Verwenden Sie dies, um die Höhe des Kreises zu ändern. Dadurch entsteht vertikal eine ovale Form. STANDARD: 1"
+
+L["Opt_GlobalFont%_name"] = "Globale Schriftgröße %"
+L["Opt_GlobalFont%_desc"] = "Dies steuert die Gesamtschriftgröße für die Benutzeroberfläche. Eine Einstellung von 50% entspricht 50% der aktuellen Symbolgröße. STANDARD: %50"
+
+L["Opt_FontSizeOverrides_name"] = "Schriftgrößenüberschreibungen"
+
+L["Opt_count_name"] = "Anzahl"
+L["Opt_ready_name"] = "Bereit"
+L["Opt_CoolDown_name"] = "Abklingzeit"
+L["Opt_timer_name"] = "Timer"
+L["Opt_linked_name"] = "Verknüpft"
+L["Opt_fontAdjustWarning_name"] = "--- Schalten Sie den Konfigurationsmodus ein, während Sie die untenstehenden Parameter anpassen ---"
+
+L["Opt_DEFAULT2_desc"] = "STANDARD: 2"
+
+L["Opt_radialSpells_desc"] = "Radiale Zauber"
+
+L["Opt_fagDesc_desc"] = "--Verwenden Sie dies, um die Zauber auszuwählen, die im primären und sekundären radialen Rahmen überwacht werden sollen.--"
+
+L["Opt_hidePassiveSpells_name"] = "Passive Zauber ausblenden"
+
+L["Opt_primarySpellOrder_name"] = "Primäre Zauberreihenfolge"
+L["Opt_primarySpells_name"] = "Primäre Zauber"
+L["Opt_secondarySpellOrder_name"] = "Sekundäre Zauberreihenfolge"
+L["Opt_secondarySpells_name"] = "Sekundäre Zauber"
+
+L["Opt_linkedSpells_name"] = "Verknüpfte Zauber"
+
+L["Opt_warlock_name"] = "Hexenmeister"
+
+L["Opt_PetFrames_name"] = "Begleiter-Fenster"
+L["Opt_HidePetFrames_name"] = "Begleiter-Fenster ausblenden"
+L["Opt_HidePetFrames_desc"] = "Zeige/Verberge die Begleiter-Fenster des Hexenmeisters."
+
+L["Opt_IconSize_name"] = "Symbolgröße"
+L["Opt_IconSize_desc"] = "Verwenden Sie dies, um die Größe der Zauber-Symbole zu ändern."
+
+L["Opt_ShardFrames_name"] = "Splitter-Fenster"
+
+L["Opt_OutOfShardsFrameSize_name"] = "Größe des 'Out of Shards'-Fensters"
+L["Opt_OutOfShardsFrameSize_desc"] = "Verwenden Sie dies, um das Fenster zu ändern, das den 'Out of Shards'-Kreis anzeigt. Schalten Sie den Konfigurationsmodus für dies ein!"
+
+L["Opt_ShardTrackerFrameSize_name"] = "Größe des Splitter-Tracker-Fensters"
+L["Opt_ShardTrackerFrameSize_desc"] = "Verwenden Sie dies, um das Fenster zu ändern, das das benutzerdefinierte Splitter-Tracker-Fenster anzeigt."
+
+L["Opt_ShardTrackerTrans_name"] = "Transparenz des Splitter-Fensters"
+L["Opt_ShardTrackerTrans_desc"] = "Verwenden Sie dies, um die Transparenz des Splitter-Fensters zu ändern. Schalten Sie den Konfigurationsmodus dafür ein."
+
+L["Opt_HideShardTracker_name"] = "Splitter-Fenster ausblenden"
+L["Opt_HideShardTracker_desc"] = "Zeige/Verberge das Splitter-Tracker-Fenster des Hexenmeisters."
+
+L["Opt_RDY_name"] = "RDY"
+L["Opt_NOSSSTR_name"] = "N/A"
+L["Opt_OOR_name"] = "OOR"
