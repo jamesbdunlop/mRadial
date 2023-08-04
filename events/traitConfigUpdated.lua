058f305199e46e8b2dfce094af6ce15e2cb436f9
James Dunlop <kryptick@iinet.net.au>
2023-08-04 21:45:01 +1000

    trait file added

---
+++ events/traitConfigUpdated.lua
@@ -0,0 +1,19 @@
+--UNIT_SPELLCAST_SUCCEEDED
+function mRadial:SPELLS_CHANGED(self, ...)
+    -- print("Player spells changed, updating mRadial spells now...")
+    -- print("Building active and secondary now")
+    mRadial:UpdateActivePrimarySpells()
+    mRadial:UpdateActiveSecondarySpells()
+    MROptionsTable.args.spellOptions.args.primarySpells.args = mRadial:BuildSpellSelectionPane("isActive", true)
+    MROptionsTable.args.spellOptions.args.secondarySpells.args = mRadial:BuildSpellSelectionPane("isSecondaryActive", true)
+    -- print("InitUI")
+    mRadial:InitUI(true)
+    -- print("Set pet frames.")
+    mRadial:SetPetFramePosAndSize()
+    mRadial:BuildPrimarySpellOrder(false)
+    mRadial:BuildSecondarySpellOrder(false)
+    -- print("Updating active and secondary now")
+    mRadial:UpdateUI(true)
+end
+
+mRadial:RegisterEvent("SPELLS_CHANGED")
