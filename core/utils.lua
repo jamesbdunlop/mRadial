11b318fa1067e62f02d998d87a391e0c29404304
James Dunlop <kryptick@iinet.net.au>
2023-12-09 12:54:52 +1100

    removed the isusable as it creates unpredicable results

--- core/utils.lua
+++ core/utils.lua
@@ -139,8 +139,7 @@ function mRadial:GetAllSpells(activeTable)
         if name == mRadial:GetSpecName() or name == UnitClass("player") then
             for x=offset+1, offset + numSpells do
                 local spellName, rank, icon, castingTime, minRange, maxRange, spellID, originalIcon = GetSpellInfo(x, "spell")
-                local isUsable1, isUsable2 = select(1, IsUsableSpell(spellID))
-                if isUsable1 or isUsable2 then
+                if spellID and spellName then
                     -- SOME WEIRD BUG WITH SHADOWFURY the names don't match??! 
                     -- Yet you print it and it's the same fkin name!
                     if spellID == 30283 then spellName = SHADOWFURY_SPELLNAME end
